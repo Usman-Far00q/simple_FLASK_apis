98293942fa29ade54ead8bc4c8264624aeeada17
Usman Farooq <usmanfarooqwork@gmail.com>
2023-08-19 19:18:36 +0500

    made the logic such that converted images are NOT saved in server DISK before sending to client

--- apis/image_converter/views.py
+++ apis/image_converter/views.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 from flask import request, jsonify, send_file
 from PIL import Image
 
@@ -19,11 +21,11 @@ def convert_image():
             if input_image.mode != 'RGB':
                 input_image = input_image.convert('RGB')
 
-            output_filename = f'converted_image.{output_format.lower()}'
-            output_image = input_image.copy()  # Make a copy before saving with the new format
-            output_image.save(output_filename, format=output_format)
-
-            return send_file(output_filename, as_attachment=True)
+            output_buffer = BytesIO()
+            # convert and save image inside python-memory (Byte-stream) instead of on DISK
+            input_image.save(output_buffer, format=output_format.lower())
+            output_buffer.seek(0)
+            return send_file(output_buffer, mimetype=f"image/{output_format.lower()}")
 
         return jsonify({'error': 'No file uploaded'}), 400
 
